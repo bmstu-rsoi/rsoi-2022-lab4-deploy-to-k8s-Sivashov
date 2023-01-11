a7bc67733fdb500616a8ef9c2f42a9a27ffed1db
Sivashov <ya.siv11@yandex.ru>
2023-01-11 04:09:10 +0300

    Try to init db data

--- scripts/fill-db.sh
+++ scripts/fill-db.sh
@@ -1,6 +1,3 @@
-#docker ps -a
 
-#docker exec -i postgres psql -U postgres postgres < ./db-v1.sql
-# postgresPod=$(kubectl get pods -l app=postgres -o=jsonpath='{.items[].metadata}' | jq -r '.name')
 
 cat db-v1.sql           | kubectl exec -i postgres-0 -- psql -U postgres -d postgres
